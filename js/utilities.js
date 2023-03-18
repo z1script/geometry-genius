4877c56da9032bc2b107cbf9cb07e6d810db5100
Zihadul Islam <zynscriptofficial@gmail.com>
2023-03-19 02:37:40 +0600

    error handler added

--- js/utilities.js
+++ js/utilities.js
@@ -2,5 +2,8 @@ function getInputValuById(inputId) {
   const getInputValueField = document.getElementById(inputId);
   const inputFieldValueString = getInputValueField.value;
   const inputFieldValue = parseFloat(inputFieldValueString);
+  if (isNaN(inputFieldValue) === true || inputFieldValue <= 0) {
+    return alert("Please Enter A Valid Number");
+  }
   return inputFieldValue;
 }
